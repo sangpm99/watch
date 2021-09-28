2e85cbcebce51c2225ffe483ae6514706ee4a9df
sangpm99 <sangpm72@wru.vn>
2021-09-28 14:59:41 +0700

    Add first

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- icon -->
+    <link rel="icon" href="https://img.icons8.com/emoji/48/000000/watch-emoji.png" type="image/x-icon" sizes="16x16"/>
+
+    <!-- icon 2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ephesis&display=swap" rel="stylesheet">
+
+    <!-- Css -->
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <title>Cửa hàng đồng hồ</title>
+</head>
+<body>
+    <!-- Begin header -->
+    <div id="header" class="header">
+        <img src="./assets/images/header.png" alt="" class="header-bg">
+        <div class="header-logo">
+            <img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-watch-interface-kiranshastry-lineal-kiranshastry.png"/>
+            <h2 class="logo-title">WATCH STORE</h2>
+        </div>
+        <div class="header-navbar">
+            <ul class="ul-navbar">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="">Thông tin</a></li>
+                <li><a href="">Sản phẩm</a></li>
+                <li><a href="">Liên hệ</a></li>
+                <li><a href="">Đăng nhập</a></li>
+            </ul>
+        </div>
+        <div class="header-search">
+            <i class="fas fa-search"></i>
+        </div>
+    </div>
+    <!-- End header -->
+
+    <!-- Begin slider -->
+    <div class="slider">
+        <h1 class="slider-title text-center text-white font-eph">
+            The right time for life
+        </h1>
+    </div>
+    <!-- End slider -->
+
+    <!-- Begin About -->
+        
+    <!-- End About -->
+</body>
+</html>
